454cf61acae11ad5ff7ac92f329de1209d75a2b0
harvidsen <hakon.arvidsen@eviny.no>
2024-03-28 23:06:17 +0100

    Add the ground
    The main charactar is now called guy

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,50 @@
 use comfy::*;
 
+
+const WIDTH: f32 = 15.0; // Default camera zoom is 30, https://comfyengine.org/book/camera/
+const GROUND: f32 = -5.0; // Ground level, so we can draw a line at the bottom of the
+const GUY_SIZE: f32 = 0.5; // Size of the guy (wow, thanks Copilot)
+
 comfy_game!("AWorld", AWorld);
 
-pub struct AWorld {
+pub struct Soul {
    pub x: f32,
    pub y: f32,
 }
 
+pub struct AWorld {
+   pub guy: Soul,
+}
+
 impl GameLoop for AWorld {
    fn new(_c: &mut EngineState) -> Self {
-      Self { x: 0.0, y: 0.0 }
+      Self { guy: Soul { x: 0.0, y: 0.0 } }
    }
 
    fn update(&mut self, _c: &mut EngineContext) {
-      draw_circle(vec2(self.x, self.y), 0.5, RED, 0);
+      draw_line(
+         vec2(-WIDTH, GROUND),
+         vec2(WIDTH, GROUND),
+         0.1,
+         WHITE,
+         0
+      );
+      draw_circle(vec2(self.guy.x, self.guy.y), GUY_SIZE, RED, 0);
 
 
       if is_key_down(KeyCode::Right) {
-         self.x += 0.5;
+         self.guy.x += 0.5;
       }
       if is_key_down(KeyCode::Left) {
-         self.x -= 0.5;
+         self.guy.x -= 0.5;
       }
       if is_key_down(KeyCode::Up) {
-         self.y += 0.5;
+         self.guy.y += 0.5;
       }
       if is_key_down(KeyCode::Down) {
-         self.y -= 0.5;
+         if self.guy.y > GROUND + GUY_SIZE {
+            self.guy.y -= 0.5;
+         }
       }
    }
 }
