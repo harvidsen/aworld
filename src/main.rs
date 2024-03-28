ee899cd9358f3ea13d767eb7bcf82d2b0dcc9b1b
harvidsen <hakon.arvidsen@eviny.no>
2024-03-28 22:32:09 +0100

    Make dot movable

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,32 @@
 use comfy::*;
 
-simple_game!("Nice red circle", update);
+comfy_game!("AWorld", AWorld);
 
-fn update(_c: &mut EngineContext) {
-   draw_circle(vec2(0.0, 0.0), 0.5, RED, 0);
+pub struct AWorld {
+   pub x: f32,
+   pub y: f32,
+}
+
+impl GameLoop for AWorld {
+   fn new(_c: &mut EngineState) -> Self {
+      Self { x: 0.0, y: 0.0 }
+   }
+
+   fn update(&mut self, _c: &mut EngineContext) {
+      draw_circle(vec2(self.x, self.y), 0.5, RED, 0);
+
+
+      if is_key_down(KeyCode::Right) {
+         self.x += 0.5;
+      }
+      if is_key_down(KeyCode::Left) {
+         self.x -= 0.5;
+      }
+      if is_key_down(KeyCode::Up) {
+         self.y += 0.5;
+      }
+      if is_key_down(KeyCode::Down) {
+         self.y -= 0.5;
+      }
+   }
 }
