f7727993fcfd5906c6204942106700a91b531ab2
harvidsen <hakon.arvidsen@eviny.no>
2024-03-28 21:49:58 +0100

    Set up comfy game engine with wgpu and vulkan

--- flake.nix
+++ flake.nix
@@ -11,9 +11,24 @@
         cargo
         rustc
         rustup
+
+        # Required for cargo build
         gcc
+        pkg-config
+        alsa-lib
+
+        # APIs  to display window
+        xorg.libX11
+        xorg.libXcursor
+        xorg.libXrandr
+        xorg.libXi
+        libxkbcommon
+
+        vulkan-loader # Grapics API
+
       ];
       RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
     };
   };
 }
